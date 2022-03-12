f82f6841a98ddf3faa216b3fd6b859d085bd6fbd
Sekoni Oluwaseun <sekonioluwaseun@gmail.com>
2022-03-12 14:08:50 +0100

    create admin model

---
+++ app/Models/Admin.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\UUID;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+
+class Admin extends Authenticatable
+{
+    use HasFactory;
+    use SoftDeletes;
+    use UUID;
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+}
