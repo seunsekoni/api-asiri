6a0021adb0023edfa874283c6c145b72aec4d7a1
Sekoni Oluwaseun <sekonioluwaseun@gmail.com>
2022-03-13 01:46:06 +0100

    create enum for queue names

---
+++ app/Enums/NamedQueue.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Enums;
+
+use BenSampo\Enum\Enum;
+
+/**
+ * @method static static MAILERS()
+ */
+final class NamedQueue extends Enum
+{
+    public const MAILERS = 'mailers';
+}
