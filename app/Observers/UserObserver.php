d10ad2b2eb503803193ae1b3cf83f84b25d0c26b
Sekoni Oluwaseun <sekonioluwaseun@gmail.com>
2022-03-24 23:46:16 +0100

    use observer to create unique number for users

---
+++ app/Observers/UserObserver.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Observers;
+
+use App\Actions\GenerateUniqueCharactersAction;
+use App\Models\User;
+
+class UserObserver
+{
+    /**
+     * Handle the User "creating" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function creating(User $user)
+    {
+        $generateUniqueCharactersAction = new GenerateUniqueCharactersAction();
+        $idNumber = $generateUniqueCharactersAction->execute('users', 'unique_id');
+        $user->unique_id = $idNumber;
+    }
+
+    /**
+     * Handle the User "created" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function created(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the User "updated" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function updated(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the User "deleted" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function deleted(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the User "restored" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function restored(User $user)
+    {
+        //
+    }
+
+    /**
+     * Handle the User "force deleted" event.
+     *
+     * @param  \App\Models\User  $user
+     * @return void
+     */
+    public function forceDeleted(User $user)
+    {
+        //
+    }
+}
--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -34,7 +34,7 @@ class EventServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        \App\Models\User::observe(\App\Observers\UserObserver::class);
     }
 
     /**
